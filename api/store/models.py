2833b909375b13ad44ac54a6789ddbe1dc7e4c47
kirubel <kiztopia.2020@gmail.com>
2021-12-21 19:11:08 +0300

    app register on the admin site

--- api/store/models.py
+++ api/store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.aggregates import Max
 
 # Create your models here.
+
+class Product(models.Model):
+    id = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
