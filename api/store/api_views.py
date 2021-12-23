84a1a8caf44c618dc8317c9aaaab703f510566d1
kirubel <kiztopia.2020@gmail.com>
2021-12-23 15:58:55 +0300

    pagination added to the productview

--- api/store/api_views.py
+++ api/store/api_views.py
@@ -2,7 +2,13 @@ from rest_framework.generics import ListAPIView
 from store.serializer import ProductSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from store.models import Product
+
+
+class ProductsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
 
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
@@ -10,4 +16,5 @@ class ProductList(ListAPIView):
     filter_backends = (DjangoFilterBackend,SearchFilter)
     filter_fields = ('id', )
     search_fields = ('name', 'price') 
+    pagination_class = ProductsPagination
 
