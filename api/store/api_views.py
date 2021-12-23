c90a33a60266c78cd7de8ce6b201ff0895ed6da9
kirubel <kiztopia.2020@gmail.com>
2021-12-23 17:56:40 +0300

    item update delete retrive method added

--- api/store/api_views.py
+++ api/store/api_views.py
@@ -1,8 +1,11 @@
-from rest_framework.generics import ListAPIView
-from store.serializer import ProductSerializer
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import response
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.exceptions import ValidationError 
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
+
+from store.serializer import ProductSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 from store.models import Product
 
 
@@ -18,3 +21,37 @@ class ProductList(ListAPIView):
     search_fields = ('name', 'price') 
     pagination_class = ProductsPagination
 
+class ProductCreate(CreateAPIView):
+    serializer_class = ProductSerializer
+
+    def create(self, request, *args,  **kwargs):
+        try: 
+            price = request.data.get('price') 
+            if price is not None and float(price) <= 0.0:
+                raise ValidationError({'price': 'Must be above $0.00'})
+        except ValueError:
+            raise ValidationError({'price': "A valid number is required"})
+        return super().create(request, *args, **kwargs )
+
+class ReteriveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    lookup_field = 'id'
+    serializer_class = ProductSerializer
+
+    def delete(self, request, *args, **kwargs):
+        product_id = request.data.get('id')
+        response = super().delete(request, *args, **kwargs)
+        if response.status_code == 200:
+            from django.core.cache import cache
+            cache.delete('product_data_{}'.format(product_id))
+        return response
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        if response.status_code == 200:
+            from django.core.cache import cache
+            product = response.data
+            cache.set('product_data_{}'.format(product['id']), {
+                'name': product['name'],
+                'price': product['price']
+            })
+        return response
--- api/store/urls.py
+++ api/store/urls.py
@@ -3,8 +3,10 @@ from rest_framework import routers
 
 
 
-from store.api_views  import  ProductList
+from store.api_views  import  ProductList, ProductCreate, ReteriveUpdateDestroy
 
 urlpatterns = [
-    path('/', ProductList.as_view())
+    path('/', ProductList.as_view()),
+    path('/add', ProductCreate.as_view() ),
+    path('/<int:id>/', ReteriveUpdateDestroy.as_view() )
 ]
