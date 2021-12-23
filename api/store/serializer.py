526c556279d0609f1e23e117ba7ec6cc2714d3ba
kirubel <kiztopia.2020@gmail.com>
2021-12-23 18:10:17 +0300

    product serializer updated

--- api/store/serializer.py
+++ api/store/serializer.py
@@ -4,9 +4,10 @@ from rest_framework import serializers
 from store.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Product
-        fields = ( 'id', 'name', 'price')
+        fields = ( 'id', 'name', 'price', )
 
     # def to_representation(self, instance):
     #     data = super().to_representation(instance)
